2c228ca8bd4ad08bf9f665c8649b3c52a3ce1ee7
delbaze <56109313+delbaze@users.noreply.github.com>
2023-05-19 13:30:24 +0200

    persist

--- src/app/store.js
+++ src/app/store.js
@@ -1,10 +1,39 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
 import themeReducer from "../theme/themeSlice";
 import demoApiReducer from "../features/demoApi/demoApiSlice";
+import storage from "redux-persist/lib/storage";
+
+import {
+  persistStore,
+  persistReducer,
+  // FLUSH,
+  // REHYDRATE,
+  // PAUSE,
+  // PERSIST,
+  // PURGE,
+  // REGISTER,
+} from "redux-persist";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const rootReducer = combineReducers({
+  theme: themeReducer,
+  api: demoApiReducer,
+});
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    theme: themeReducer,
-    api: demoApiReducer,
-  },
+  reducer: persistedReducer,
+  // middleware: (getDefaultMiddleware) =>
+  //   getDefaultMiddleware({
+  //     serializableCheck: {
+  //       ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+  //     },
+  //   }),
 });
+
+export const persistor = persistStore(store);
